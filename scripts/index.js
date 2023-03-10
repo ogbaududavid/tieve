6043566117967d30b78b24be5ba2e851b9027484
ogbaududavid <ogbaudu.dc@gmail.com>
2023-03-10 09:50:20 +0100

    added more nav animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 function toggleMenu(){
 	document.getElementsByClassName("links")[0].classList.toggle("responsive");
+	document.getElementsByClassName("menuButton")[0].style.display="none";
+	document.getElementsByClassName("logo")[0].style.gridTemplateColumns ="9fr 1fr";
+}
+function toggleMenu2(){
+	document.getElementsByClassName("links")[0].classList.toggle("responsive");
+	document.getElementsByClassName("menuButton")[0].style.display="block";
+	document.getElementsByClassName("logo")[0].style.gridTemplateColumns ="1fr 8fr 1fr";
 }
