fea865daee22e6b72ce557fce03a4f4484df9c7a
ogbaududavid <ogbaudu.dc@gmail.com>
2023-04-07 00:20:04 +0100

    added margin and styling

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 function toggleMenu(){
 	document.getElementsByClassName("links")[0].classList.toggle("responsive");
 	document.getElementsByClassName("menuButton")[0].style.display="none";
-	document.getElementsByClassName("logo")[0].style.gridTemplateColumns ="9fr 1fr";
-	document.getElementsByClassName("logo")[0].style.justifyItems ="right";
+	document.getElementsByClassName("menuButton2")[0].style.display="block";
+	document.getElementsByClassName("menuButton2")[0].style.width="70px";
+	document.getElementsByClassName("menuButton2")[0].style.padding="5px";
+	document.getElementsByClassName("logo")[0].style.gridTemplateColumns ="1fr 1fr 9fr";
+	document.getElementsByClassName("logo")[0].style.justifyItems ="left";
 }
 function toggleMenu2(){
 	document.getElementsByClassName("links")[0].classList.toggle("responsive");
 	document.getElementsByClassName("menuButton")[0].style.display="block";
 	document.getElementsByClassName("menuButton")[0].style.width="70px";
+	document.getElementsByClassName("menuButton2")[0].style.display="none";
 	document.getElementsByClassName("logo")[0].style.gridTemplateColumns ="1fr 8fr 1fr";
 	document.getElementsByClassName("logo")[0].style.justifyItems ="left";
 	document.getElementsByClassName("point4")[0].style.justifySelf ="right";
+}
+function toggleHome(){
+	document.getElementsByClassName("homeCategories").toggle("responsive3");
 }
