9bfcb67f5445ffdcbc7faed149cebc98ea757a69
ogbaududavid <ogbaudu.dc@gmail.com>
2023-04-07 09:20:17 +0100

    added styling to cart page

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ function toggleMenu(){
 	document.getElementsByClassName("menuButton2")[0].style.display="block";
 	document.getElementsByClassName("menuButton2")[0].style.width="70px";
 	document.getElementsByClassName("menuButton2")[0].style.padding="5px";
+	document.getElementsByClassName("menuButton2")[0].style.fontSize = "1.5rem";
 	document.getElementsByClassName("logo")[0].style.gridTemplateColumns ="1fr 1fr 9fr";
 	document.getElementsByClassName("logo")[0].style.justifyItems ="left";
+	document.getElementsByClassName("point4")[0].style.justifySelf ="right";
 }
 function toggleMenu2(){
 	document.getElementsByClassName("links")[0].classList.toggle("responsive");
